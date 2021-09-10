0ee0043fd6e2c1cfea5b568ece6862d6c2570819
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-10 14:58:49 +0100

    feat: add Todo component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>
-    <h1>My Todo</h1>
-    <div className='card'>
-      <h2>Title</h2>
-      <div className='actions'>
-        <button className='btn'>Delete</button>
-      </div>
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <Todo /> 
     </div>
-  </div>;
+  );
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo() {
+    return (
+        <div>
+            <div className='card'>
+            <h2>Title</h2>
+            <div className='actions'>
+                <button className='btn'>Delete</button>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
