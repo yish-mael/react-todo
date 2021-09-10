5986e5151a8b5d9a8cee21d4cdf51b712bb0b06b
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-10 16:23:53 +0100

    feat: add Backdrop and Modal components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todo</h1>
-      <Todo text='Learn React' /> 
+      <Todo text='Learn React' />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 function  Modal() {
     return (
-        <div>
+        <div className='modal'>
             <p>Are you sure?</p>
             <button className='btn btn--alt'>Cancel</button>
             <button className='btn'>Confirm</button>
