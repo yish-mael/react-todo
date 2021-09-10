e0cef427e84b63c611ce117ff819736a995cf929
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-10 14:28:02 +0100

    feat: add basic style to index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Hello!</div>
+  return <div>
+    <h1>My Todo</h1>
+    <div>
+      <h2>Title</h2>
+      <div>
+        <button>Delete</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
